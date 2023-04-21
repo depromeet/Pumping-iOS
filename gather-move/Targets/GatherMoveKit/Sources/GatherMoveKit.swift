738680fe964c27988e8905ed2318edaac9950c46
송영모 <mooyoung2309@gmail.com>
2023-04-21 17:07:50 +0900

    refactor: 불필요한 타겟 파일들 제거

--- gather-move/Targets/GatherMoveKit/Sources/GatherMoveKit.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public final class GatherMoveKit {
-    public static func hello() {
-        print("Hello, from your Kit framework")
-    }
-}
--- gather-move/Targets/GatherMoveKit/Tests/GatherMoveKitTests.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import XCTest
-
-final class GatherMoveKitTests: XCTestCase {
-    func test_example() {
-        XCTAssertEqual("GatherMoveKit", "GatherMoveKit")
-    }
-}
