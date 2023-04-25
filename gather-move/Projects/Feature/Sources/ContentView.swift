563432b59b739f347bf3135d8a1b640e1e202665
송영모 <mooyoung2309@gmail.com>
2023-04-26 02:06:01 +0900

    feat: wip main 실행 안되는 문제

--- gather-move/Projects/Feature/Sources/ContentView.swift
+++ gather-move/Projects/Feature/Sources/ContentView.swift
@@ -13,6 +13,7 @@ public struct ContentView: View {
     
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
     }
 }
 
