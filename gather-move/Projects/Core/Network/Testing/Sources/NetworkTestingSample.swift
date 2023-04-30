924c588377cb03139d215a5080fa9839bd0a45a9
송영모 <mooyoung2309@gmail.com>
2023-05-01 00:02:36 +0900

    feat: 프로젝트 이름을 pumping으로 변경

--- gather-move/Projects/Core/Network/Testing/Sources/NetworkTestingSample.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkTestingSample.swift
-//  NetworkInterface
-//
-//  Created by 송영모 on 2023/04/26.
-//
-
-import Foundation
--- gather-move/Projects/Core/Utility/Sources/UtilitySample.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UtilitySample.swift
-//  Utility
-//
-//  Created by 송영모 on 2023/04/26.
-//
-
-import Foundation
