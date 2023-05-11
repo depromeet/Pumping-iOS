958ebcd095abf19afbca8a4feb9791d978207922
송영모 <mooyoung2309@gmail.com>
2023-05-11 13:06:20 +0900

    feat: swift 디펜던시 추가

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -9,6 +9,7 @@ import ProjectDescription
 
 let SPM = SwiftPackageManagerDependencies([
     .remote(url: "https://github.com/pointfreeco/swift-composable-architecture", requirement: .upToNextMajor(from: "0.52.0")),
+    .package(url: "https://github.com/airbnb/swift", from: "1.0.0"),
 ])
 
 let dependencies = Dependencies(
