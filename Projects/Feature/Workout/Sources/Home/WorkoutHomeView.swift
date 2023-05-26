cef240274843e8a1f3eb079e97f05963731417b3
송영모 <mooyoung2309@gmail.com>
2023-05-26 15:40:53 +0900

    feat: 타이틀, 서브 타이틀 구현

--- Projects/Feature/Workout/Sources/Home/WorkoutHomeView.swift
+++ Projects/Feature/Workout/Sources/Home/WorkoutHomeView.swift
@@ -10,26 +10,30 @@ import SwiftUI
 import ComposableArchitecture
 
 import FeatureWorkoutInterface
+import SharedDesignSystem
 
 extension WorkoutHomeView: View {
     public var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
-            VStack {
+            VStack(spacing: .zero) {
                 HStack {
                     Text("어떤 운동을 할 예정인가요?")
-                        .font(.system(size: 24))
-                        .fontWeight(.bold)
+                        .font(.pretendard(size: 24, type: .bold))
+                        .foregroundColor(.colorGrey900)
                     
                     Spacer()
                 }
+                .padding(.horizontal)
+                .padding(.bottom, 12)
                 
                 HStack {
                     Text("오늘 할 운동을 모두 선택해 주세요")
-                        .font(.system(size: 15))
-                        .fontWeight(.medium)
+                        .font(.pretendard(size: 15, type: .medium))
+                        .foregroundColor(.colorGrey600)
                     
                     Spacer()
                 }
+                .padding(.horizontal)
                 
                 Text("전신")
                 
@@ -46,7 +50,8 @@ extension WorkoutHomeView: View {
                 }
                 .padding(.bottom, 4)
             }
-            .padding(.horizontal)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.colorGrey100)
         }
     }
 }
