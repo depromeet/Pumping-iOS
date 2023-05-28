0d3b42843c644f89d05a01d71d084b0118a3c88c
derrickkim0109 <derrickkim0109@naver.com>
2023-05-28 12:42:30 +0900

    feat: Store state, action 추가

--- Projects/Feature/Onboarding/Interface/Sources/OtherProfileDetail.swift
+++ Projects/Feature/Onboarding/Interface/Sources/OtherProfileDetail.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 public struct OtherProfileDetailStore: ReducerProtocol {
     private let reducer: Reduce<State, Action>
@@ -16,6 +17,7 @@ public struct OtherProfileDetailStore: ReducerProtocol {
     
     public struct State: Equatable {
         public let index: Int
+        public let week: [Week] = Week.data
         
         public init(index: Int) {
             self.index = index
@@ -24,6 +26,9 @@ public struct OtherProfileDetailStore: ReducerProtocol {
     
     public enum Action: BindableAction, Equatable {
         case binding(BindingAction<State>)
+
+        case tapHeartButton
+        case tapComparisonButton
     }
     
     public var body: some ReducerProtocol<State, Action> {
@@ -39,4 +44,19 @@ public struct OtherProfileDetailView {
     public init(store: StoreOf<OtherProfileDetailStore>) {
         self.store = store
     }
+}
+
+public struct Week: Hashable, Identifiable {
+    public let id = UUID()
+    public let day: String
+}
+
+extension Week {
+    static let data = [Week(day: "월"),
+                       Week(day: "화"),
+                       Week(day: "수"),
+                       Week(day: "목"),
+                       Week(day: "금"),
+                       Week(day: "토"),
+                       Week(day: "일"),]
 }
