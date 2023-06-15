7e7c846c3bb5699144fb6649c60c4dee1bc6e027
derrickkim0109 <derrickkim0109@naver.com>
2023-06-15 14:06:08 +0900

    refactor: 이미지 네이밍 변경 및 Extension으로 이미지 분리

---
+++ Projects/Shared/DesignSystem/Sources/Images.swift
@@ -0,0 +1,19 @@
+//
+//  Images.swift
+//  SharedDesignSystem
+//
+//  Created by Derrick kim on 2023/06/15.
+//
+
+import SwiftUI
+
+public extension Image {
+    static let arrow = SharedDesignSystemAsset.Images.arrow.swiftUIImage
+    static let arrowForPercentage = SharedDesignSystemAsset.Images.arrowForPercentage.swiftUIImage
+    static let boy = SharedDesignSystemAsset.Images.boy.swiftUIImage
+    static let clock = SharedDesignSystemAsset.Images.clock.swiftUIImage
+    static let widthOfChangeCalorie = SharedDesignSystemAsset.Images.widthOfChangeCalorie.swiftUIImage
+    static let widthOfChangeWorkoutTime = SharedDesignSystemAsset.Images.widthOfChangeWorkoutTime.swiftUIImage
+    static let widthOfChangeHearRate = SharedDesignSystemAsset.Images.widthOfChangeHearRate.swiftUIImage
+    static let widthOfChangeMaxWorkoutPart = SharedDesignSystemAsset.Images.widthOfChangeMaximumPart.swiftUIImage
+}
