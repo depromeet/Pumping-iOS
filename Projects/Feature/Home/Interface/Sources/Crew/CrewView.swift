71156d57e1ffca0b59103a6e82044f692b9282d2
derrickkim0109 <derrickkim0109@naver.com>
2023-06-18 17:11:17 +0900

    feat: CrewView 생성

---
+++ Projects/Feature/Home/Interface/Sources/Crew/CrewView.swift
@@ -0,0 +1,22 @@
+//
+//  CrewView.swift
+//  FeatureHomeInterface
+//
+//  Created by Derrick kim on 2023/06/15.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+public struct CrewView: View {
+    public let store: StoreOf<CrewStore>
+
+    public init(store: StoreOf<CrewStore>) {
+        self.store = store
+    }
+
+    public var body: some View {
+        Text("Crew View!")
+            .foregroundColor(.white)
+    }
+}
