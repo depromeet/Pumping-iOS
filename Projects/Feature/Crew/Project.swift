a0f6b708474a635b65057432def812f3f2634957
송영모 <mooyoung2309@gmail.com>
2023-06-22 13:06:57 +0900

    fix: profile interface 디펜던시 추가

--- Projects/Feature/Crew/Project.swift
+++ Projects/Feature/Crew/Project.swift
@@ -9,7 +9,8 @@ let project = Project.makeModule(
             interface: .Crew,
             factory: .init(
                 dependencies: [
-                    .domain
+                    .domain,
+                    .feature(interface: .Profile)
                 ]
             )
         ),
