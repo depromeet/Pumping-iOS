5190aacae0ec370aef59cbca12e5e01bb5bd8f90
송영모 <mooyoung2309@gmail.com>
2023-06-23 15:10:48 +0900

    fix: 타이머 무한 동작 해결

--- Projects/Shared/Util/Sources/Managers/DateManager.swift
+++ Projects/Shared/Util/Sources/Managers/DateManager.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class DateManager {
+public struct DateManager {
     public static func toClockString(from second: Int) -> String {
         let h = String(format: "%02d", second / 3600)
         let m = String(format: "%02d", (second % 3600) / 60)
