a35b66076302038c4d43808c041ffe3cf373287c
derrickkim0109 <derrickkim0109@naver.com>
2023-06-23 23:15:44 +0900

    feat: CrewRankingView 구현

--- Projects/Feature/Crew/Interface/Sources/Root/CrewRootView.swift
+++ Projects/Feature/Crew/Interface/Sources/Root/CrewRootView.swift
@@ -11,13 +11,13 @@ import FeatureProfileInterface
 
 public struct CrewRootView: View {
     public let store: StoreOf<CrewRootStore>
-
+    
     public init(
         store: StoreOf<CrewRootStore>
     ) {
         self.store = store
     }
-
+    
     public var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
             NavigationStack(path: viewStore.binding(\.$path)) {
@@ -33,16 +33,19 @@ public struct CrewRootView: View {
                         case .crewRanking:
                             IfLetStore(self.store.scope(state: \.crewRanking, action: { .crewRanking($0) })) {
                                 CrewRankingView(store: $0)
+                                    .toolbarRole(.editor)
                             }
                             
                         case .profile:
                             IfLetStore(self.store.scope(state: \.profile, action: { .profile($0) })) {
                                 ProfileView(store: $0, profileSubject: .my)
+                                    .toolbarRole(.editor)
                             }
-
+                            
                         case .widthOfChange:
                             IfLetStore(self.store.scope(state: \.widthOfChange, action: { .widthOfChange($0) })) {
                                 WidthOfChangeView(store: $0)
+                                    .toolbarRole(.editor)
                             }
                         }
                     }
